8f9704f10729f254429bf6e588bb9f2914bede5d
GaryTsai <jaylp33@gmail.com>
2020-02-26 22:49:30 +1100

    jaylp33
    home polishing

---
+++ js/all.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('#fullpage').fullpage({
+        anchors:['about', 'exp', 'skill', 'task', 'contact'],
+        scrollOverflow: true,
+        menu: '#header',
+    });
+
+});
