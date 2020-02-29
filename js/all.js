3f17d1e00796669b906e5f625e3748dce9cd1630
GaryTsai <jaylp33@gmail.com>
2020-02-29 23:56:03 +1100

    animation for index
    heading to about page

--- js/all.js
+++ js/all.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    defaultAnimation();
+
     var windowWidth = $( window ).width();
     var liWidth = document.getElementById('topNav').offsetWidth;
 
@@ -34,7 +36,6 @@ $(document).ready(function () {
     // HambergerMenu
     $(document).on('click', '.hamburgerMenu', function() {
 
-
         if ($(this).hasClass('.hamburgerActive')) {
             $('.topNav').removeClass('.hamburgerActive');
             $('.hamburgerMenu').removeClass('.hamburgerActive');
@@ -42,31 +43,36 @@ $(document).ready(function () {
             $('.mobileMenu').css({
                 'right': 0
             });
-    
-           
-
         } else {
             $('.topNav').addClass('.hamburgerActive');
             $('.hamburgerMenu').addClass('.hamburgerActive');
 
-            
-    
             $('.mobileMenu').css({
                 'right' : liWidth + 'px'
             })
         }
-        
-
-        
     });
+
+    
+
+
+    
 
     $(window).on('hashchange', function(e){
         // Your Code goes here
         var scroll = window.location.href;
 
         // get section when find #
-        var urlPage = scroll.split('#');
-        console.log(urlPage[1]);
+        urlPage = scroll.split('#');
+
+        Page = urlPage[1];
+
+        if (Page == 'index') {
+            showName_index();
+        } 
+        else if (Page == 'about') {
+        }
+        
        });
 
     $(window).scroll(function(){
@@ -77,7 +83,9 @@ $(document).ready(function () {
     })
 
 
-
+    $(document).on('click', '#cardBtn', function(){
+        $.fn.fullpage.moveSectionDown();
+      });
     
 
 
@@ -86,17 +94,84 @@ $(document).ready(function () {
 
     
 
-    // CardTitle Name anime
-    var name = anime({
-        targets: '.cardTitle polygon, path, rect',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInOutSine',
-        duration: 1500,
-        delay: function(el, i) { return i * 50 },
-        direction: 'alternate',
-        loop: false,
-          
-        
-      });
+    
+
+
+
+
+
+
+
+
+    // index name animation
+    function showName_index() {
+        var myName = anime.timeline({
+            easing: 'easeInOutQuad',
+            direction: 'alternate',
+            loop: false
+          });
+    
+          myName.add({
+              targets: '.cardTitle polygon, path, rect',
+                  strokeDashoffset: [anime.setDashoffset, 0],
+                  easing: 'easeInOutSine',
+                  duration: 1500,
+                  delay: function(el, i) { return i * 10 },
+                  direction: 'alternate',
+                  loop: false,
+          })
+          .add({
+            targets: '.myName',
+            width: '100%',
+            height: '100%',
+            easing: 'easeInOutExpo',
+            duration: 1500
+          }, '-=1500')
+          .add({
+              targets: '.divLine',
+              translateY: '-90%',
+              easing: 'easeInOutSine',
+              begin: function() {
+                  
+                  $('.divLine').css({
+                      'border-top': '#d6b161 4px solid'
+                  });
+              }
+          }, '-=200')
+          .add({
+              targets: '.cardDes',
+              translateY: '-50%',
+              begin: function(){
+                  $('.cardDes').css({
+                      'opacity': 1
+                      
+                  });
+              }
+          },'+=200')
+          .add({
+              target: '.cardBtn',
+              begin: function(){
+                  $('.cardBtn').css({
+                      'opacity': 1
+                  });
+              },
+              translateY: '-50%',
+          }, '+=500')
+    }
+    
+   // default animation
+   function defaultAnimation() {
+
+    var Page = window.location.href;
+    var urlPage = Page.split('about-me/');
+    var urlPage2 = urlPage[1].split('.');
+    var onPage = urlPage2[0];
+    
+    if (onPage == 'index') {
+        showName_index();
+    }
+}
+      
+      
 
 });
